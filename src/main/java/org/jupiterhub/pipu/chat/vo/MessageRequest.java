3a351bcef6b7c0eff6614e6da0c09066bdcac23d
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2023-06-05 01:28:12 +0900

    build: Added lombok dependency. This might increase the size of the project, optimize later

--- src/main/java/org/jupiterhub/pipu/chat/vo/MessageRequest.java
+++ src/main/java/org/jupiterhub/pipu/chat/vo/MessageRequest.java
@@ -1,7 +1,11 @@
 package org.jupiterhub.pipu.chat.vo;
 
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 
+
+@Data
 public class MessageRequest {
 
     @NotBlank(message = "from is required")
@@ -11,27 +15,4 @@ public class MessageRequest {
     @NotBlank(message = "message is required")
     private String message;
 
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
